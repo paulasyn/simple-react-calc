87c225f811a727b4f01491d31c50a2b8623de8aa
Paul Asyn <pasyn3@gatech.edu>
2024-03-14 18:12:47 -0700

    handleButtonPress impemented

--- src/Calculator.tsx
+++ src/Calculator.tsx
@@ -8,12 +8,39 @@ import Input from "./components/Input";
 export default function Calculator() {
     const [input, setInput] = useState<string | null>(null);
     const [total, setTotal] = useState<number>(0);
-    const [hasError, setHasError] = useState<boolean>(false)
+    const [hasError, setHasError] = useState<boolean>(false);
+
+    const [oldIput, setOldInput] = useState<string | null>(null);
+    const [showOldInput, setShowOldInput] = useState<boolean>(false);
+    const [prevSymbol, setPrevSymbol] = useState<string | null>(null);
+    const [equalSignPressed, setEqualSignPressed] = useState<boolean>(false);
 
 
+    function storeNumToScreen(num: string){
+        setInput(prev => prev === '0' || prev === null || equalSignPressed && input?.charAt(0) !== '.'? num: prev + num)
+    }
+
+    function handleSymbol(symbol: string){
+        // TODO: Implement logic to handle different button types (symbol, numbers)
+    }
+    
     function handleButtonPress(value: string): void {
-    // TODO: Implement logic to handle different button types (symbol, numbers)
-
+        if (showOldInput) {
+            setShowOldInput(false);
+        }
+        const numValue: number = parseInt(value);
+        if (Number.isNaN(numValue)) {
+            handleSymbol(value);
+        } 
+        else {
+            storeNumToScreen(value);
+            if (equalSignPressed) {
+                setTotal(0)
+            }
+        }
+        if (value !== '=') {
+            setEqualSignPressed((value === '+/-' && equalSignPressed || value === '%' || value === '.' && equalSignPressed))
+        }
     }
     return (
         <div className='calculator-container'>
